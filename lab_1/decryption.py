1ce5f87db0ce0a7f3c95e658ede49c9a6781df97
Alina <ashavarina07@gmail.com>
2024-02-15 21:20:01 +0400

    The second task is ready

--- lab_1/decryption.py
+++ lab_1/decryption.py
@@ -11,11 +11,25 @@ def text_analysis(path: str, new_path: str) -> None:
     """Осуществляет анализ зашифрованного текста; принимает путь к файлу 
     с исходным текстом, путь к новому файлу, в который пойдет запись"""
     text = open_file(path)
-    dict_my = dict() 
+    dict_my = dict()
     for i in text:
         dict_my[i] = text.count(i) / len(text)
     sort = dict(sorted(dict_my.items(), key=lambda x: x[1], reverse=True))
-    saving_values(sort, new_path)  
+    saving_values(sort, new_path)
+
+
+def decryption(path: str, keys: str, new_path: str) -> None:
+    """Осуществляет расшифровку исходного текста; принимает путь к файлу с исходным 
+    текстом, ключ шифрования и путь к новому файлу, в который пойдет запись"""
+    try:
+        text = open_file(path)
+        with open(keys, "r", encoding="utf-8") as file:
+            dict = json.load(file)
+        for key, value in dict.items():
+            text = text.replace(key, value)
+        write_text(new_path, text)
+    except Exception as ex:
+        logging.error(f"The directory could not be opened: {ex.message}\n{ex.args}\n")
 
 
 if __name__ == "__main__":
@@ -24,4 +38,9 @@ if __name__ == "__main__":
     text_analysis(
         os.path.join(main["directory"], main["folder_2"], main["code_text"]),
         os.path.join(main["directory"], main["folder_2"], main["text_analysis"]),
-    ) 
+    )
+    decryption(
+        os.path.join(main["directory"], main["folder_2"], main["code_text"]),
+        os.path.join(main["directory"], main["folder_2"], main["cipher_key"]),
+        os.path.join(main["directory"], main["folder_2"], main["decrypted"]),
+    )
