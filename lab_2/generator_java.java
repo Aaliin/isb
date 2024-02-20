a813cbfe219a7afccfd7681552c33cb6cd60df72
Alina <ashavarina07@gmail.com>
2024-02-20 13:40:39 +0400

    The first part is completed

--- lab_2/generator_java.java
+++ lab_2/generator_java.java
@@ -1,18 +1,17 @@
-class Main {
-    public static int a = 41;
-    public static int c = 11119;
-    public static int m = 11113;
-    public static int seed = 1;
+import java.util.Random;
 
+class Main {
     public static void getNextRandom() {
         for (int i = 0; i < 100; i++) {
-            seed = (a * seed + c) % m; 
-            System.out.print(seed % 2);
+            Random random = new Random();
+            int seed = random.nextInt();
+            System.out.print(Math.abs(seed % 2));
         }
     }
 
-    public static void main(String[] args) { 
-        getNextRandom(); 
+    public static void main(String[] args) {
+        getNextRandom();
     }
 }
 // 1101010101000001110101110001111100100001000011100010010010000100001000111110111110101010001101000010
+// 1100010011110111100000000000000100001001010011110110001110110111111101011101001011100100010100010100
