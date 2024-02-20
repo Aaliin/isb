c2f07108083bd96b953b62b3b7b3bafee8264b57
Alina <ashavarina07@gmail.com>
2024-02-20 21:39:46 +0400

    The Atbash cipher is executed

--- lab_1/encryption.py
+++ lab_1/encryption.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 RUS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
 
 
-def encrypt(path: str, new_path: str, step: int) -> None:
+def encrypt(path: str, new_path: str) -> None:
     """Осуществляет шифрование текста; принимает путь к файлу с исходным 
     текстом, путь к новому файлу, в который пойдет запись, и шаг для сдвига
     """
@@ -20,15 +20,14 @@ def encrypt(path: str, new_path: str, step: int) -> None:
             if i == " ":
                 cipher_str += i
                 continue
-            place = RUS[::-1].find(i)
-            new_place = (place + step - 1) % len(RUS)
-            cipher_str += RUS[new_place]
+            place = (RUS[::-1].find(i)) % len(RUS)
+            cipher_str += RUS[place]
         write_text(new_path, cipher_str)
     except Exception as ex:
         logging.error(f"Data could not be encrypted: {ex.message}\n{ex.args}\n")
 
 
-def cipher_key(path: str, step: int) -> None:
+def cipher_key(path: str) -> None:
     """Осуществляет сохранение ключа от зашифрованного текста; принимает 
     путь к файлу, в который пойдет запись, и шаг для сдвига 
     """
@@ -36,45 +35,18 @@ def cipher_key(path: str, step: int) -> None:
         key = dict()
         for i in RUS:
             place = RUS[::-1].find(i)
-            key[i] = RUS[(place + step - 1) % len(RUS)]
+            key[i] = RUS[place % len(RUS)]
         saving_values(key, path)
     except Exception as ex:
-        logging.error(f"Data is not recognized: {ex.message}\n{ex.args}\n")
-
-
-def decryptor(path: str, new_path: str, step: int) -> None:
-    """Осуществляет расшифровку зашифрованного текста; принимает 
-    путь к файлу, в который пойдет запись, и шаг для сдвига 
-    """
-    try:
-        data = open_file(path)
-        cipher_str = ""
-        for i in data:
-            if i == " ":
-                cipher_str += i
-                continue
-            place = RUS[::-1].find(i)
-            new_place = (place - step + 1) % len(RUS)
-            cipher_str += RUS[new_place]
-        write_text(new_path, cipher_str)
-    except Exception as ex:
-        logging.error(f"An error occurred during decryption: {ex.message}\n{ex.args}\n")
-
+        logging.error(f"Data is not recognized: {ex.message}\n{ex.args}\n") 
 
 if __name__ == "__main__":
     with open(os.path.join("lab_1", "main.json"), "r") as file:
         main = json.load(file)
     encrypt(
         os.path.join(main["directory"], main["folder_1"], main["initial_text"]),
-        os.path.join(main["directory"], main["folder_1"], main["my_encrypted"]),
-        main["step"],
+        os.path.join(main["directory"], main["folder_1"], main["my_encrypted"]), 
     )
     cipher_key(
-        os.path.join(main["directory"], main["folder_1"], main["cipher_key"]),
-        main["step"],
-    )
-    decryptor(
-        os.path.join(main["directory"], main["folder_1"], main["my_encrypted"]),
-        os.path.join(main["directory"], main["folder_1"], main["decrypted"]),
-        main["step"],
-    )
+        os.path.join(main["directory"], main["folder_1"], main["cipher_key"]), 
+    ) 
