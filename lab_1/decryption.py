26a7c550d38201b931998249da808967c8ec6601
Alina <ashavarina07@gmail.com>
2024-03-11 17:06:57 +0400

    Completing the second task

--- lab_1/decryption.py
+++ lab_1/decryption.py
@@ -12,22 +12,29 @@ def text_analysis(path: str, new_path: str) -> None:
     """Осуществляет анализ зашифрованного текста; принимает путь к файлу 
     с исходным текстом, путь к новому файлу, в который пойдет запись
     """
-    text = open_file(path)
-    dict_my = dict()
-    for i in text:
-        dict_my[i] = text.count(i) / len(text)
-    sort = dict(sorted(dict_my.items(), key=lambda x: x[1], reverse=True))
-    saving_values(sort, new_path)
+    try:
+        text = open_file(path)
+        dict_my = {}
+        for i in text:
+            dict_my[i] = dict_my.get(i, 0) + 1
+        dict_my = {k: (v/len(text)) for k, v in dict_my.items()}
+        sort = dict(sorted(dict_my.items(), key=lambda x: x[1], reverse=True))
+        saving_values(sort, new_path)
+    except Exception as ex:
+        logging.error(f"The directory could not be opened: {ex.message}\n{ex.args}\n")
 
 
-def decoded(old_str: str, new_str: str, text: str, path: str, dict: dict) -> str:
+def decoded(old_str: str, new_str: str, text: str, path: str, dict: dict) -> None:
     """Осуществляет замену символов в тексте; принимает исходный символ, символ на 
     который заменяем, зашифрованный текст, путь к файлу, в который пойдет запись 
     """
-    text = text.replace(old_str, new_str)
-    dict[old_str] = new_str
-    saving_values(dict, path)
-    return text
+    try:
+        dict[old_str] = new_str
+        for char in dict.keys():
+            text = text.replace(char, dict[char])
+        saving_values(dict, path)
+    except Exception as ex:
+        logging.error(f"The values could not be changed: {ex.message}\n{ex.args}\n")
 
 
 def decryption(path: str, new_path: str, keys: str) -> None:
@@ -37,16 +44,16 @@ def decryption(path: str, new_path: str, keys: str) -> None:
     try:
         text = open_file(path)
         with open(keys, "r", encoding="utf-8") as file:
-            dict = json.load(file)
-        for key, value in dict.items():
-            text = text.replace(key, value)
+            dict_my = json.load(file)
+        for item in dict_my.items():
+            text = text.replace(item[0], item[1])
         write_text(new_path, text)
     except Exception as ex:
         logging.error(f"The directory could not be opened: {ex.message}\n{ex.args}\n")
 
 
 if __name__ == "__main__":
-    with open(os.path.join("lab_1", "setting.json"), "r") as file:
+    with open(os.path.join("lab_1", "settings.json"), "r") as file:
         settings = json.load(file)
     text_analysis(
         os.path.join(settings["directory"], settings["folder_2"], settings["code_text"]),
