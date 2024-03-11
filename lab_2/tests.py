3b2b05afd0799b2eb28c3ae075aa922c315063be
Alina <ashavarina07@gmail.com>
2024-03-11 19:15:31 +0400

    The work report is ready

--- lab_2/tests.py
+++ lab_2/tests.py
@@ -1,7 +1,8 @@
 import json
 import logging
 import math
-import os
+import mpmath
+import os 
 
 
 logging.basicConfig(level=logging.INFO)
@@ -21,13 +22,13 @@ def frequency_bitwise_test(data: str) -> float:
 
 def same_consecutive_bits(data: str) -> float:
     sum_n = 0
+    v_n = 0
     try:
         sum_n = data.count("1")/len(data)
         comparison = abs(sum_n-0.5)
         module = 2/math.sqrt(len(data))
         if comparison >= module:
             return 0
-        v_n = 0
         for j in range(len(data)-1):
             if data[j] != data[j+1]:
                 v_n += 1
@@ -41,22 +42,32 @@ def same_consecutive_bits(data: str) -> float:
 def longest_sequence_of_units(data: str) -> float:
     block_length = 8
     n_i = [0.2148, 0.3672, 0.2305, 0.1875]
+    value = [0]*4
+    xi = 0
     try:
         for i in range(0, len(data), block_length):
-            value = [0]*4
+            count = 0
+            prev = 0
             for num in data[i:i+block_length]:
-                # print(num)
                 if num != "0":
+                    count += 1
+                else:
+                    if count > prev:
+                        prev = count
+                    count = 0
+                max_num = max(count, prev)
+            match max_num:
+                case max_num if max_num <= 1:
                     value[0] += 1
-            print(data[i], value)
-            xi = 0
-
-            print(value)
-            # for j in range(len(n_i)):
-            # print(n_i[j])
-            # xi += pow(value[j]-16*n_i[j],2)/(16*n_i[j])
-        # p_value = math.igamc(3/2, xi/2)
-            p_value = 0
+                case 2:
+                    value[1] += 1
+                case 3:
+                    value[2] += 1
+                case max_num if max_num >= 4:
+                    value[3] += 1
+        for j in range(len(n_i)):
+            xi += pow(value[j]-16*n_i[j], 2)/(16*n_i[j])
+            p_value = mpmath.gammainc(3/2, xi/2)
         return p_value
     except Exception as ex:
         logging.error(f"Division by zero: {ex.message}\n{ex.args}\n")
@@ -67,7 +78,7 @@ if __name__ == "__main__":
         settings = json.load(file)
     c = settings["c++"]
     java = settings["java"]
-    with open(os.path.join(settings["directory"], settings["result"]), "w", encoding="utf-8") as main:
+    with open(os.path.join("lab_2", "nist_result.txt"), "w", encoding="utf-8") as main:
         main.write(f"test_frequency_bitwise:\n\tc++: {frequency_bitwise_test(c)}\n\t")
         main.write(f"java: {frequency_bitwise_test(java)}\n")
         main.write(f"test_same_consecutive_bits:\n\tc++: {same_consecutive_bits(c)}\n\t")
